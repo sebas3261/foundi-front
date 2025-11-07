bdf40e150c6641cabda06d8c39de7220001215a5
sebas3261 <sebassanchez3261@gmail.com>
2025-11-07 17:17:54 -0500

    title color

--- app/[locale]/(nav)/page.tsx
+++ app/[locale]/(nav)/page.tsx
@@ -7,14 +7,20 @@ export default function page() {
   return (
     <main>
       <section className="flex justify-center items-center flex-col">
-        <div className="flex flex-row gap-2 items-center font-semibold bg-[#E9EAF8] text-[#6065E3] py-1 px-2 rounded-md border-1 border-[#D5D5F5] mt-10">
+        <div className="flex flex-row gap-2 items-center font-semibold bg-[#E9EAF8] text-[#6065E3] py-1 px-2 rounded-md border-1 border-[#D5D5F5] mt-10 ">
           <Icon icon="hugeicons:stars" className="w-[10px] h-[10px]" />
           <p className="text-[10px]">{t("best")}</p>
         </div>
 
         <div className="mt-5">
           <h2 className="text-[40px] font-bold text-center leading-tight">
-            <span className="bg-gradient-to-b from-[#6065E3] via-[#A7A9F2] to-[#6065E3] bg-clip-text text-transparent">
+            <span
+              className="
+      bg-gradient-to-b from-[#6065E3] via-[#A7A9F2] to-[#6065E3]
+      bg-clip-text text-transparent
+      dark:bg-none dark:text-[#6065E3]
+    "
+            >
               {t("title1")}
             </span>
             <span className="block text-black dark:text-white">
